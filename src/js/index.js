5fe6a0041a5ad7172c152ad9f3b2f8f4ca98eaad
Hellsing13 <maritzalara215@gmail.com>
2023-06-15 22:31:34 -0600

    burger

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,33 @@ import css from "../css/styles.css";
 import "bootstrap";
 import "../css/bootstrap.css";
 
-const get_meal_btn = document.getElementsById("get_meal");
-get_meal_btn.adEventListener("click", () => {
+
+
+const Aleatorio = document.getElementById("Random");
+const almacenComida = document.getElementById("platillo");
+
+Aleatorio.addEventListener("click", () => {
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
         .then(res => res.json())
         .then(res => {
-            createMeal(res.meals[0]);
+            nuevoPlatillo(res.meals[0]);
         })
         .catch(e => {
             console.warn(e);
         });
-})
+});
+
+const nuevoPlatillo = (platillo) => {
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+        if (platillo[`strIngredient${i}`]) {
+            ingredients.push(`${platillo[`strIngredient${i}`]} - ${platillo[`strMeasure${i}`]}`);
+        } else {
+            break;
+        }
+    }
+
+    const Comida = <h1 src='${platillo.strIngredient4}'></h1>;
+
+    almacenComida.innerHTML = Comida;
+}
